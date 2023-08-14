5dfd3a6c28924b59dcb27ba4c88f30b6b824efb6
aspeelucas <aspeelucas@gmail.com>
2023-08-14 17:58:20 -0300

    First Commit

---
+++ src/pages/layouts/NotFound.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import NotFoundImg from '../../assets/images/404.png';
+const NotFound = () => {
+return (
+<>
+<img src={NotFoundImg} alt="imagen error 404"/>
+</>
+)
+}
+export default NotFound
